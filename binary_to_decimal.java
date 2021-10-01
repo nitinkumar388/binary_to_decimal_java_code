0646dded7acd8b53bf4639f80256f3da33ce46fc
Nitin Kumar <85660316+nitinkumar388@users.noreply.github.com>
2021-10-01 22:23:23 +0530

    Add files via upload

---
+++ binary_to_decimal.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.Scanner.*;
+
+class binarytodecimal {
+
+	static int binaryToDecimal(String n)
+	{
+		//Write your logic here
+	}
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		String temp = sc.nextLine();
+		System.out.println(binaryToDecimal(temp));
+	}
+}
